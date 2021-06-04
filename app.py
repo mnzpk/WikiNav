9203d168695de1b9b187a4406a1337773a89861e
mnzpk <84433140+mnzpk@users.noreply.github.com>
2021-06-04 11:19:04 +0500

    fix caching

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import pandas as pd
 app = Flask(__name__)
 cache = Cache(
     app,
-    config={'CACHE_TYPE': 'redis',
+    config={'CACHE_TYPE': 'RedisCache',
             'CACHE_REDIS_HOST': 'tools-redis',
             'CACHE_KEY_PREFIX': 'wn-api-test'}
 )
