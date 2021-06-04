4c75298a3faac6a70d15aaac02865957d97a684e
mnzpk <84433140+mnzpk@users.noreply.github.com>
2021-06-04 13:19:27 +0500

    fix app.py

--- app.py
+++ app.py
@@ -16,21 +16,20 @@ cache = Cache(
 LANGUAGES = ('pt', 'en')
 
 
-@app.route('/<language>/<title>/<any("sources", "destinations"):dir>')
-def main(language, title, dir):
+@app.route('/<language>/<title>/<any("sources", "destinations"):direction>')
+def main(language, title, direction):
     if language not in LANGUAGES:
         abort(404)
-
     start = request.args.get('start', 1, type=int)
     limit = request.args.get('limit', 20, type=int)
     sort = request.args.get('sort', 'desc')
     df = get_clickstream_data(language, title)
-    df.sort_values(by='n', ascending=sort == 'asc', inplace=True)
-    if dir == 'sources':
+    if direction == 'sources':
         df = df[df.curr == title][['prev', 'n']]
     else:
         df = df[df.prev == title][['curr', 'n']]
     df.columns = ['title', 'views']
+    df.sort_values(by='views', ascending=sort == 'asc', inplace=True)
     return get_paginated_response(df, start, limit, title, request.base_url, sort)
 
 
@@ -38,7 +37,6 @@ def main(language, title, dir):
 def get_paginated_response(df, start, limit, title, url, sort):
     if start > len(df):
         abort(404)
-
     results = df.iloc[start-1:start+limit-1]
     if start + limit > len(df):
         next = ''
