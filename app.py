351a290dba6ecb31535ea4cc78fcc9cf17d59f35
mnzpk <84433140+mnzpk@users.noreply.github.com>
2021-06-11 11:58:53 +0500

    Expose Total-Count header in response to cross-origin requests

--- app.py
+++ app.py
@@ -9,7 +9,10 @@ import pandas as pd
 app = Flask(__name__,
             static_url_path='',
             static_folder='client')
+
+app.config['CORS_EXPOSE_HEADERS'] = 'Total-Count'
 CORS(app)
+
 cache = Cache(
     app,
     config={'CACHE_TYPE': 'RedisCache',
