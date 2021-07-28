0ca98ae8189523944955cc0538e59a81381a00c9
mnzpk <84433140+mnzpk@users.noreply.github.com>
2021-07-28 13:54:37 +0500

    add section anchor links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { FiLink } from 'react-icons/fi';
 import { SearchStateProvider } from './searchStateContext';
 import Instruction from './components/Instruction';
 import LanguageComparison from './components/LanguageComparison';
@@ -35,7 +36,12 @@ const App = () => (
           </div>
         </div>
         <div className="content section">
-          <h2 className="section-text">Visualizing Reader Navigation</h2>
+          <h2 className="section-text" id="Visualizing_Reader_Navigation">
+            <a className="section-anchor" href="#Visualizing_Reader_Navigation">
+              <FiLink size="20px" />
+            </a>
+            Visualizing Reader Navigation
+          </h2>
           <Sankey name="title" />
           <Instruction className="paragraph">
             <strong>Reading the chart: </strong>
@@ -46,20 +52,38 @@ const App = () => (
               help contextualize these pageviews.
             </span>
           </Instruction>
-          <h2 className="section-text">Pageviews by Referrer</h2>
+          <h2 className="section-text" id="Pageviews_by_Referrer">
+            <a className="section-anchor" href="#Pageviews_by_Referrer">
+              <FiLink size="20px" />
+            </a>
+            Pageviews by Referrer
+          </h2>
           <TreeMap />
           <Instruction className="paragraph">
             <strong>Reading the chart: </strong>
             <span>
-              The chart shows common pathways to and from the selected title.
-              The links between nodes represent the number of pageviews.
-              Hovering over the nodes and links gives additional information to
-              help contextualize these pageviews.
+              Incoming clickstream traffic is mapped to a set of
+              {' '}
+              <a href="https://meta.wikimedia.org/wiki/Research:Wikipedia_clickstream#Data_Preparation">
+                referrers
+              </a>
+              . This chart shows the percentage of views each of these referrers
+              directs to the selected title.
             </span>
           </Instruction>
-          <h2 className="section-text">Comparison Across Languages</h2>
+          <h2 className="section-text" id="Comparison_Across_Languages">
+            <a className="section-anchor" href="#Comparison_Across_Languages">
+              <FiLink size="20px" />
+            </a>
+            Comparison Across Languages
+          </h2>
           <LanguageComparison languages={languages} />
-          <h2 className="section-text">Summary Tables</h2>
+          <h2 className="section-text" id="Summary_Tables">
+            <a className="section-anchor" href="#Summary_Tables">
+              <FiLink size="20px" />
+            </a>
+            Summary Tables
+          </h2>
           <Instruction className="paragraph">
             <strong>Reading the tables: </strong>
             <span>
