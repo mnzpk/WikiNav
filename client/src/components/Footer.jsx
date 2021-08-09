6bdc9401d8c0a25c60ec23b068be28976347a486
mnzpk <84433140+mnzpk@users.noreply.github.com>
2021-08-09 17:59:02 +0500

    add `Footer` component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Footer = () => {
+  const attributions = [
+    ['Muniza', 'https://www.github.com/mnzpk'],
+    ['Martin (WMF)', 'https://meta.wikimedia.org/wiki/User:MGerlach_(WMF)'],
+    ['Isaac (WMF)', 'https://meta.wikimedia.org/wiki/User:Isaac_(WMF)'],
+  ];
+  return (
+    <footer className="wmf-footer">
+      <section>
+        <span className="footer-attributions">
+          Developed by{' '}
+          {attributions
+            .map(([name, link]) => (
+              <a href={link}>
+                <strong>{name}</strong>
+              </a>
+            ))
+            .reduce((prev, curr, index) => [
+              prev,
+              index === attributions.length - 1 ? ' and ' : ', ',
+              curr,
+            ])}
+          .
+        </span>
+        <br />
+        <a href="https://www.github.com/mnzpk/wikinav">View Source</a>
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
