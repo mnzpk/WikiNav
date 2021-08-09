b6c2f4fd06bdac5820b93c2bcd2c34a866c6211e
mnzpk <84433140+mnzpk@users.noreply.github.com>
2021-08-09 18:14:41 +0500

    use `Header`,  `Footer` and `Overview`

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { withRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { FiLink } from 'react-icons/fi';
 import { SearchStateProvider } from './searchStateContext';
+import Header from './components/Header';
 import Instruction from './components/Instruction';
 import LanguageComparison from './components/LanguageComparison';
 import LanguageSearch from './components/LanguageSearch';
@@ -10,6 +11,8 @@ import Sankey from './components/Sankey';
 import SummaryTables from './components/SummaryTables';
 import TitleSearch from './components/TitleSearch';
 import TreeMap from './components/TreeMap';
+import Overview from './components/Overview';
+import Footer from './components/Footer';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -19,81 +22,86 @@ const queryClient = new QueryClient({
   },
 });
 
-const languages = [
-  { value: 'en', label: 'en.wikipedia.org' },
-  { value: 'pt', label: 'pt.wikipedia.org' },
-];
+const Heading = ({ text }) => {
+  const id = text.replaceAll(' ', '-');
+  return (
+    <h2 className="section-text" id={id}>
+      <a className="section-anchor" href={`#${id}`}>
+        <FiLink size="20px" />
+      </a>
+      {text}
+    </h2>
+  );
+};
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <SearchStateProvider>
       <div className="main-container">
-        <h1 className="section title-text">Clickstream Analysis</h1>
+        <Header />
+        <section className="title-text margin-top-3">WikiNav</section>
         <div className="controls-container">
-          <div className="section controls">
-            <LanguageSearch name="language" options={languages} />
+          <section className="controls margin-top-3">
+            <LanguageSearch name="language" />
             <TitleSearch name="title" />
-          </div>
+          </section>
         </div>
-        <div className="content section">
-          <h2 className="section-text" id="Visualizing_Reader_Navigation">
-            <a className="section-anchor" href="#Visualizing_Reader_Navigation">
-              <FiLink size="20px" />
-            </a>
-            Visualizing Reader Navigation
-          </h2>
+        <section className="content">
+          <Heading text="Overview" />
+          <Overview />
+          <Heading text="Reader Navigation" />
           <Sankey name="title" />
           <Instruction className="paragraph">
             <strong>Reading the chart: </strong>
             <span>
-              The chart shows common pathways to and from the selected title.
-              The links between nodes represent the number of pageviews.
-              Hovering over the nodes and links gives additional information to
-              help contextualize these pageviews.
+              This chart visualizes incoming and outgoing traffic, to and from
+              the selected article. The nodes at either end represent sources
+              and destinations. Hovering over them shows what percentage of
+              traffic they send to or receive from the article and clicking
+              their labels changes the selected title to that article.
             </span>
           </Instruction>
-          <h2 className="section-text" id="Pageviews_by_Referrer">
-            <a className="section-anchor" href="#Pageviews_by_Referrer">
-              <FiLink size="20px" />
-            </a>
-            Pageviews by Referrer
-          </h2>
+          <Heading text="Sources of Traffic" />
           <TreeMap />
           <Instruction className="paragraph">
             <strong>Reading the chart: </strong>
             <span>
-              Incoming clickstream traffic is mapped to a set of
-              {' '}
+              This chart visualizes the percentage of traffic each source sends
+              to the selected title. The legend shows the{' '}
               <a href="https://meta.wikimedia.org/wiki/Research:Wikipedia_clickstream#Data_Preparation">
-                referrers
-              </a>
-              . This chart shows the percentage of views each of these referrers
-              directs to the selected title.
+                naming scheme
+              </a>{' '}
+              that Wikipedia uses to identify different types of sources.
             </span>
           </Instruction>
-          <h2 className="section-text" id="Comparison_Across_Languages">
-            <a className="section-anchor" href="#Comparison_Across_Languages">
-              <FiLink size="20px" />
-            </a>
-            Comparison Across Languages
-          </h2>
-          <LanguageComparison languages={languages} />
-          <h2 className="section-text" id="Summary_Tables">
-            <a className="section-anchor" href="#Summary_Tables">
-              <FiLink size="20px" />
-            </a>
-            Summary Tables
-          </h2>
+          <Heading text="Comparison Across Languages" />
+          <LanguageComparison />
+          <Instruction className="paragraph">
+            <strong>Reading the charts: </strong>
+            <span>
+              These charts compare the composition of traffic across multiple
+              language editions of the selected title by looking up the top
+              sources and destinations for the current article in the
+              clickstream dump for the selected languages. You can select one or
+              more languages to compare to from the search bar above.
+            </span>
+          </Instruction>
+          <Heading text="Numbers in Context" />
           <Instruction className="paragraph">
             <strong>Reading the tables: </strong>
             <span>
-              These tables summarize the visualizations above. The views and
-              percentage of views columns double as heatmaps. The columns can be
-              sorted by clicking on their labels.
+              These tables summarize the visualizations above and provide
+              additional context. For incoming pageviews, the percentage of
+              pageviews is for the current page that comes from each
+              source-page. For outgoing pageviews, the percentage of pageviews
+              is for the target page that comes from the current page.
+              <br />
+              To sort the table by a certain column, click the column header.
             </span>
           </Instruction>
           <SummaryTables />
-        </div>
+        </section>
+        <Footer />
       </div>
     </SearchStateProvider>
   </QueryClientProvider>
