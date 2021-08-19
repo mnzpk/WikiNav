a670fa02f8ab8715b9fc6c2f32b15c8625d098a6
mnzpk <84433140+mnzpk@users.noreply.github.com>
2021-08-19 14:37:56 +0500

    add disclaimer to footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { GiPlainCircle } from 'react-icons/gi';
 
 const Footer = () => {
   const attributions = [
@@ -9,7 +10,7 @@ const Footer = () => {
   return (
     <footer className="wmf-footer">
       <section>
-        <span>
+        <div>
           Developed by{' '}
           {attributions
             .map(([name, link]) => (
@@ -23,9 +24,26 @@ const Footer = () => {
               curr,
             ])}
           .
-        </span>
-        <br />
-        <a href="https://www.github.com/mnzpk/wikinav">View Source</a>
+        </div>
+        <a href="https://www.github.com/mnzpk/wikinav">Source code</a>
+        <GiPlainCircle size="4px" className="footer-separator" />
+        <a href="https://www.github.com/mnzpk/wikinav/blob/main/README.md#license">
+          License
+        </a>
+        <GiPlainCircle size="4px" className="footer-separator" />
+        <a href="https://wikitech.wikimedia.org/wiki/Portal:Toolforge">
+          Toolforge
+        </a>
+        <div className="disclaimer">
+          No guarantees are made that this tool will be maintained.
+          <br />
+          No additional personal data is collected by this tool per the Cloud
+          Services{' '}
+          <a href="https://wikitech.wikimedia.org/wiki/Wikitech:Cloud_Services_Terms_of_use">
+            Terms of Use
+          </a>
+          .
+        </div>
       </section>
     </footer>
   );
